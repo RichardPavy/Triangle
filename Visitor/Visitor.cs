71138ae1e686af14c442c3788e80af7818b91e8b
Richard Pavy <richard.pavy@gmail.com>
2020-04-06 23:23:21 -0700

    Move VisitorScope

--- Visitor/Visitor.cs
+++ Visitor/Visitor.cs
@@ -1,9 +1,7 @@
 ﻿namespace Visitors
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using Visitors.Utils;
 
     /// <summary>
     /// Parent class for class and field visitors.
@@ -53,41 +51,5 @@
     public enum VisitStatus
     {
         Continue, SkipChildren, Exit
-    }
-
-    public struct VisitorScope<TData>
-    {
-        public readonly VisitStatus Status;
-        public readonly Action After;
-        public readonly Optional<TData> Data;
-
-        private VisitorScope(VisitStatus status, Action after)
-        {
-            Status = status;
-            After = after;
-            Data = Optional<TData>.Null;
-        }
-
-        private VisitorScope(VisitStatus status, Action after, Optional<TData> data)
-        {
-            Status = status;
-            After = after;
-            Data = data;
-        }
-
-        public static implicit operator VisitorScope<TData>(Action after) =>
-            new VisitorScope<TData>(VisitStatus.Continue, after);
-        public static implicit operator VisitorScope<TData>(VisitStatus status) =>
-            new VisitorScope<TData>(status, null);
-
-        public static VisitorScope<TData> operator +(VisitorScope<TData> scope, Action andThen)
-        {
-            return new VisitorScope<TData>(scope.Status, scope.After + andThen, scope.Data);
-        }
-
-        public static VisitorScope<TData> operator +(VisitorScope<TData> scope, Optional<TData> data)
-        {
-            return new VisitorScope<TData>(scope.Status, scope.After, data);
-        }
     }
 }
---
+++ Visitor/VisitorScope.cs
@@ -0,0 +1,41 @@
+﻿namespace Visitors
+{
+    using System;
+    using Visitors.Utils;
+
+    public struct VisitorScope<TData>
+    {
+        public readonly VisitStatus Status;
+        public readonly Action After;
+        public readonly Optional<TData> Data;
+
+        private VisitorScope(VisitStatus status, Action after)
+        {
+            Status = status;
+            After = after;
+            Data = Optional<TData>.Null;
+        }
+
+        private VisitorScope(VisitStatus status, Action after, Optional<TData> data)
+        {
+            Status = status;
+            After = after;
+            Data = data;
+        }
+
+        public static implicit operator VisitorScope<TData>(Action after) =>
+            new VisitorScope<TData>(VisitStatus.Continue, after);
+        public static implicit operator VisitorScope<TData>(VisitStatus status) =>
+            new VisitorScope<TData>(status, null);
+
+        public static VisitorScope<TData> operator +(VisitorScope<TData> scope, Action andThen)
+        {
+            return new VisitorScope<TData>(scope.Status, scope.After + andThen, scope.Data);
+        }
+
+        public static VisitorScope<TData> operator +(VisitorScope<TData> scope, Optional<TData> data)
+        {
+            return new VisitorScope<TData>(scope.Status, scope.After, data);
+        }
+    }
+}
