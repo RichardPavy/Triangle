9741e60ff61dc3b5f20f824fc98ed00294f96a68
Richard <richard.pavy@gmail.com>
2020-11-14 16:05:30 -0800

    Update IMergeJoinIterable.cs

--- LevelDB/Iterables/IMergeJoinIterable.cs
+++ LevelDB/Iterables/IMergeJoinIterable.cs
@@ -2,16 +2,16 @@ namespace Triangle.LevelDB.Iterables
 {
     using Triangle.LevelDB.Iterators;
 
-    public interface IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue>
-        : IIterable<JoinEntry<TLeftKey, TRightKey>, JoinEntry<TLeftValue, TRightValue>>
+    public interface IMergeJoinIterable<TK1, TK2, TV1, TV2>
+        : IIterable<JoinEntry<TK1, TK2>, JoinEntry<TV1, TV2>>
     {
         IMergeJoinIterable<TLeftKey2, TRightKey2, TLeftValue2, TRightValue2> Cast<TLeftKey2, TRightKey2, TLeftValue2, TRightValue2>();
-        new IMergeJoinIterator<TLeftKey, TRightKey, TLeftValue, TRightValue> GetIterator();
-        new IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue> Reverse();
-        IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue> Range(TLeftKey fromLeft, TRightKey fromRight, TLeftKey toLeft, TRightKey toRight);
-        IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue> Prefix(TLeftKey prefixLeft, TRightKey prefixRight);
-        new IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue> Snapshot();
-        new IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue> FillCache(bool fillCache);
-        new IMergeJoinIterable<TLeftKey, TRightKey, TLeftValue, TRightValue> VerifyChecksums(bool verifyChecksums);
+        new IMergeJoinIterator<TK1, TK2, TV1, TV2> GetIterator();
+        new IMergeJoinIterable<TK1, TK2, TV1, TV2> Reverse();
+        IMergeJoinIterable<TK1, TK2, TV1, TV2> Range(TK1 fromLeft, TK2 fromRight, TK1 toLeft, TK2 toRight);
+        IMergeJoinIterable<TK1, TK2, TV1, TV2> Prefix(TK1 prefixLeft, TK2 prefixRight);
+        new IMergeJoinIterable<TK1, TK2, TV1, TV2> Snapshot();
+        new IMergeJoinIterable<TK1, TK2, TV1, TV2> FillCache(bool fillCache);
+        new IMergeJoinIterable<TK1, TK2, TV1, TV2> VerifyChecksums(bool verifyChecksums);
     }
 }
